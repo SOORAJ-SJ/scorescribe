36724d473aef9fe8a5282c652391a6c42a2e400c
Sooraj M <61335345+SOORAJ-SJ@users.noreply.github.com>
2024-08-17 14:22:25 +0000

    feat: add and integrate methods to get sheets

--- src/lib/common/data.js
+++ src/lib/common/data.js
@@ -1,9 +1,20 @@
+import { changeAuthState } from "$lib/stores/auth.store";
 import { SHEETS_API_METADATA } from "./constants";
+import { getToken } from "./functions";
 
 export async function getSheets() {
     const url = new URL(`${SHEETS_API_METADATA.sheetsAPIUrl}/${SHEETS_API_METADATA.spreadsheetId}`);
     url.searchParams.append('fields', 'spreadsheetId,sheets.properties(sheetId,title)')
-    const response = await fetch(url);
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${getToken()}`
+        }
+    });
+    if ([401, 403].includes(response.status)) {
+        sessionStorage.clear();
+        changeAuthState(false);
+    }
     const data = await response.json();
     return data;
 }
