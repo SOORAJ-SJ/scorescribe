500e03fe580f5bd30abcab587581c06c52183e3f
Sooraj M <61335345+SOORAJ-SJ@users.noreply.github.com>
2024-08-18 08:06:44 +0000

    fix: add the codes missed while reverting changes

---
+++ src/lib/types.js
@@ -0,0 +1,49 @@
+/**
+ * @typedef rowValues
+ * @property {string} formattedValue
+ */
+
+/**
+ * @typedef rowData
+ * @property {Array.<rowValues>} values
+ */
+
+/**
+ * @typedef data
+ * @property {Array.<rowData>} rowData
+ */
+
+/**
+ * @typedef properties
+ * @property {string} sheetId 
+ * @property {string} title
+ * 
+ */
+
+/**
+ * @typedef property
+ * @type {object}
+ * @property {properties} properties
+ * @property {Array.<data>} data
+ * 
+ */
+
+/**
+ * @typedef spreadsheet
+ * @property {string} spreadsheetId 
+ * @property {Array.<property>} sheets
+ * @property {?any} error
+ * 
+ */
+
+/**
+ * @type {Record<string, any>}
+ */
+
+/**
+ * @typedef userScore
+ * @type {object}
+ * @property {string} email
+ * @property {number} score
+ * @property {boolean} isMainPlayer
+*/
